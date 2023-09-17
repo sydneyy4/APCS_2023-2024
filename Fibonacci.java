b7f2959d5857659bf4859bf2984c4093e491e8a1
sydneyy4 <143736206+sydneyy4@users.noreply.github.com>
2023-09-16 20:52:17 -0700

    fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,14 +1,15 @@
 public class Fibonacci
 {
     public static void main (String[] args){
-        int n1 = 0, n2 = 1, n3;
-        System.out.println(n1);
-        System.out.println(n2);
+        int a = 0, b = 1, c;
+        //print out first two terms, 0 and 1
+        System.out.println(a);
+        System.out.println(b);
         for (int i = 0; i < 15; i++){
-            n3 = n1+n2;
-            System.out.println(n3);
-            n1 = n2;
-            n2 = n3;
+            c = a+b;
+            System.out.println(c);
+            a = b;
+            b = c;
         }
         //make print 0 and 1
     }
