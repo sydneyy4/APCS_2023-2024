d930d694cb61d7354c5ba9706e23db0e4c1e1be3
sydneyy4 <143736206+sydneyy4@users.noreply.github.com>
2023-11-28 14:35:13 -0800

    unit9
    finished

--- unit9/FibonacciList.java
+++ unit9/FibonacciList.java
@@ -5,28 +5,40 @@ public class FibonacciList{
 
 		int[] fibonacci = new int[n];
 
-		if (n <= 0){
+		if (n > 0){
 			fibonacci[0] = 0;
 		}
-		else if (n > 1){
+		 if (n > 1){
 			fibonacci[1]=1;
-		
-			for (int i = 2; i < n; i++){
-				fibonacci[i] = fibonacci[i-1] - fibonacci[i-2];
-			}
-		}
+		} 
 
+		for (int i = 2; i < n; i++){
+				fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+			}
+		
 		return fibonacci;
 	}
 
 	public static void main (String [] args){
-		int[] a = fibonacci(2);
-		//int b = 3;
-		//int c = 4;
-		//int d = 5; 
-		System.out.println(" " + a);
-		//System.out.println(" " + fibonacci(b));
-		//System.out.println(" " + fibonacci(c));
-		//System.out.println(" " + fibonacci(d));
+
+		for (int f : fibonacci(2)){
+			System.out.print(" " + f);
+		}
+		System.out.println();
+
+		for (int f : fibonacci(3)){
+			System.out.print(" " + f);
+		}
+		System.out.println();
+
+		for (int f : fibonacci(4)){
+			System.out.print(" " + f);
+		}
+		System.out.println();
+
+		for (int f : fibonacci(5)){
+			System.out.print(" " + f);
+		}
+		System.out.println();
 	}
 }
